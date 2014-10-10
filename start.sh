4e0e983fc620194c69ea6d347af396d56b7b9a8c
Fabian Linzberger <e@lefant.net>
2014-10-10 07:30:25 +0200

    add -boot start_sasl to start.sh

--- start.sh
+++ start.sh
@@ -1,2 +1,2 @@
 #!/bin/bash
-erl -noshell -sname rest_server -setcookie adm -pa ebin -eval "commands:start()" &
+erl -noshell -boot start_sasl -sname rest_server -setcookie adm -pa ebin -eval "commands:start()" &
